f9d7fe2fada44f6e99ca78f16578414dc83e9f7c
Sem <s.v.arensbergen@student.tue.nl>
2025-10-09 11:26:18 +0200

    Basic gui

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
 public class Main {
+    void update() {
+        JFrame frame = new JFrame ("Scaup Ducks Log");
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        panel.setBackground(new Color(0, 0, 0));
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     public static void main(String[] args) {
-        System.out.println("it's britney bitch");
+        new Main().update();
     }
 }
