3524d80ae7aaf27e5c553482192eeab1df6f8758
Mimi301106 <m.s.petrova@student.tue.nl>
2025-10-16 11:28:30 +0200

    colums made

--- Logs.java
+++ Logs.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.Random;
+
 import javax.swing.*;
 
 public class Logs extends JPanel {
@@ -26,20 +28,21 @@ public class Logs extends JPanel {
             this.img = img;
     }
 
-    int logWidth;
-    int logHeight;
+    int logWidth = 95;
+    int logHeight = 29;
 
-    int logX;
-    int logY;
+    int logX = 0;
+    int logY = logHeight;
     ArrayList<Block> logArray;
 
     // movement specifics
 
     int gravity = 1;
+    int velocityX = 12;
 
     Timer enterLogTimer;
 
-    public Logs() {
+    public void Logs() {
 
         setPreferredSize(new Dimension( panelWidth, panelHeight));
 
@@ -51,9 +54,41 @@ public class Logs extends JPanel {
         enterLogTimer = new Timer(1500, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+               
                 enterLog();
             }
         });
         enterLogTimer.start();
     }
+
+    public void enterLog() {
+        private static final int cols = 5;
+        private static final int rows = 1;
+        final double chance = 0.4;// 40% for the image to occur
+
+        Image[] grid = new Image[cols];
+        Random random = new Random();
+
+        for (int i = 0; i < cols; i++){
+            if (random.nextDouble() < chance){
+                grid[i] = logImg;
+            } 
+            else{
+                grid[i] = null;
+            }
+        }
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+     public void draw(Graphics g) {
+
+        g.drawImage()
+
+     }
+
+
+   
 }
