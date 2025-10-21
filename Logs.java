1d88d21e9364607a6beb14b55a00a1c2f40d5e64
Mimi301106 <m.s.petrova@student.tue.nl>
2025-10-21 12:40:05 +0200

    falling logs

--- Logs.java
+++ Logs.java
@@ -1,37 +1,78 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.*;
 import java.io.IOException;
+import javax.swing.Timer;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 
-public class Logs {
+public class Logs extends JPanel {
 
     private BufferedImage logImage;
+    private final int logHeight = 100;
+    private final int logWidth = 25;
+    private int[] xPosition = {0, 101, 202, 303};
+    private int[] yPosition = {0, 0, 0, 0};
 
-    public void Logs(){
+    Timer logTimer;
+
+
+    public Logs() {
 
        
         
-         try {
+        try {
             logImage = ImageIO.read(getClass().getResource("/log.png"));
         } catch (IOException | IllegalArgumentException e) {
             System.err.println("opa");
         }
 
+        logTimer = new Timer (100, new ActionListener() { 
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            for (int i = 0; i < yPosition.length; i++) {
+                yPosition[i] += 3;
+            
+                if (yPosition[i] > 700){
+                    yPosition[i] = 0;
+                }
+            }
+            repaint();
+        }
+        });
+
+        logTimer.start();
+
+            
+        
+
     }
 
-     public void draw(Graphics g) {
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+     
 
-        if (logImage != null){
-         g.drawImage(logImage, 20, 20, null);
-        }
+    public void draw (Graphics g) {
+
+        for (int i = 0; i < xPosition.length; i++) {
+            if (logImage != null) {
+                g.drawImage(logImage, xPosition[i], yPosition[i], logHeight, logWidth, null);
+            }
+    }
 
       }
 
-      public void update() {
+    public void update() {
 
-      }
+    }
 
     
 }
